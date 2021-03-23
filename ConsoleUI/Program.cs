aaa1a2f69ccfc8f4ce2423356dbe6641e7b5c9f4
zeynepsl <zeyneprsalman@gmail.com>
2021-03-23 11:48:46 +0300

    some changes have been made

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,32 +11,36 @@ namespace ConsoleUI
         {
             CarManager carManager = new CarManager(new InMemoryCarDal());
 
+            //mevcut arabalrı listeleyelim
+            ArabaListele(carManager);
+
+            //araba ekleyelim
+            InMemoryCarDal carDal = new InMemoryCarDal();
+            Car car = new Car
+            {
+                Id = 1,
+                ColorId = 2,
+                ModelYear = new DateTime(2020),
+                DailyPrice = 350000,
+                BrandId = 1,
+                Description = "audi",
+            };
+            carDal.Add(car);
+
+            //tüm arabaları yeniden listeleyelim
+            Console.WriteLine("yeni eklenen ürünlerle oluşan yeni araba listesi: ");
+            ArabaListele(carManager);
+        }
+
+        private static void ArabaListele(CarManager carManager)
+        {
             //arabaları listeleyelim
             Console.WriteLine("mevcut araba listesi : ");
             foreach (var car1 in carManager.GetAll())
             {
                 Console.WriteLine(car1.Description);
             }
-
-            //araba ekleyelim
-            InMemoryCarDal carDal = new InMemoryCarDal();
-            Car car = new Car
-            {
-                Id = 1, ColorId=2, ModelYear=new DateTime(2020), DailyPrice=350000, BrandId=1, Description="audi",
-            };
-            
-            carDal.Add(car);
-
-
-            //tüm arabaları yeniden listeleyelim
-            Console.WriteLine("yeni eklenen ürünlerle oluşan yeni araba listesi: ");
-            foreach (var car1 in carDal.GetAll())
-            {
-                Console.WriteLine(car1.Description);
-            }
-
-            //araba silelim 
-            //carDal.Delete(car);
         }
+
     }
 }
