e45b379c220e93d2fd1a09b42e7df1333dcc1565
zeynepsl <zeyneprsalman@gmail.com>
2021-03-24 16:46:41 +0300

    Database added

---
+++ CarRentalDatabase/dbo/Tables/Car.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Car] (
+    [Id]         INT        NOT NULL,
+    [BrandId]    INT        NULL,
+    [ColorId]    INT        NULL,
+    [ModelYear]  DATETIME   NULL,
+    [DailyPrice] FLOAT (53) NULL,
+    CONSTRAINT [PK_Car] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Car_Car] FOREIGN KEY ([ColorId]) REFERENCES [dbo].[Color] ([Id])
+);
+
---
+++ CarRentalDatabase/dbo/Tables/Color.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Color] (
+    [Id]   INT           NOT NULL,
+    [Name] NVARCHAR (20) NULL,
+    CONSTRAINT [PK_Color] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
