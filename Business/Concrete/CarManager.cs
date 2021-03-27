0bbd129bb07ea5b9bf94dd82fe57f3984cde099a
zeynepsl <zeyneprsalman@gmail.com>
2021-03-27 09:47:00 +0300

    GetCarsByBrandId, GetCarsByColorId services added

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -9,8 +9,13 @@ namespace Business.Concrete
 {
     public class CarManager : ICarService
     {
-        ICarDal _carDal;
-
+         ICarDal _carDal;
+        /*business sadece bunu biliyor inmemory vs bilmiyor
+         * şu an bu inmemeory de olabilir öbür gün entityframework de olabilir, bunlar alternarif == veri erişim alternatifleri
+         * 
+         *bir iş sınıfı başka sınıfları new lemez
+         * CarManager new lendiğinde bu kurucu metot diyecek ki bana ICarDal referansı ver
+         */
         public CarManager(ICarDal carDal)
         {
             _carDal = carDal;
@@ -21,6 +26,29 @@ namespace Business.Concrete
             //iş kodları . .
             //eğer ki iş kodlarından geçilirse DataAccess e erişim yapılacak
             return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(c => c.CarId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(c => c.ColorId == id);
+        }
+
+        public void Add(Car car)
+        {
+            if(car.Description.Length > 2 && car.DailyPrice > 0)
+            {
+                //eğer ki iş kodlarından geçerse,
+                _carDal.Add(car);//DataAccess katmanına bağlanacağız.
+            }
+            else
+            {
+                Console.WriteLine("kaydetme basarisiz, lutfen urun aciklamasini 2 karakterden buyuk ve gunluk masrafi sifirdan buyuk girin");
+            }
         }
     }
 }
