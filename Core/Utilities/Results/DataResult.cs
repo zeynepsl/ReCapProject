07d269de5f37a3c682f2192b17a2db38ce4481c0
zeynepsl <zeyneprsalman@gmail.com>
2021-03-31 11:55:16 +0300

    Results configuration has been made in the core layer

---
+++ Core/Utilities/Results/DataResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class DataResult<T> : Result, IDataResult<T>
+    //Result Sınıfından farkı datasının da olması
+    //mesaj + işlem sonucu +  data (ürün, ürünler vs. . ) döndürecek
+    {
+        public DataResult(T data, bool success, string message) : base(success, message)
+        {
+            Data = data;
+        }
+
+        public DataResult(T data, bool success) : base(success)
+        {
+            Data = data;
+        }
+        public T Data { get; }
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        
+
+        public Result(bool success, string message):this(success)
+        {
+            //Success = success; bu atamayı diğer kurucu metotta da yaptık
+            //kod tekrarı yapmamak için this(success) kullandık
+            Message = message;
+        }
+
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
