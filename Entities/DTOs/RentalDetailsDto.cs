5055de32fbbd970502732d2ccbd8736a6a6e302a
zeynepsl <zeyneprsalman@gmail.com>
2021-04-01 13:21:09 +0300

    User, Customer, Rental Tables added

---
+++ Entities/DTOs/RentalDetailsDto.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+    public class RentalDetailsDto:IDto
+    {
+        public int RentalId { get; set; }
+        public int CarId { get; set; }
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CarName { get; set; }
+        public String BrandName { get; set; }
+        public String ColorName { get; set; }
+        public DateTime CarModelYear { get; set; }
+        public double CarDailyPrice { get; set; }
+        public string CarDescription { get; set; }
+        public DateTime CarRentDate { get; set; }
+        public DateTime CarReturnDate { get; set; }
+       
+    }
+}
