002b1fee002002c5e37af24296121586f3347871
zeynepsl <zeyneprsalman@gmail.com>
2021-04-01 13:28:26 +0300

    CRUD operations are done for the added tables

---
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -0,0 +1,49 @@
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfRentalDal : EfEntityRepositoryBase<Rental, EfCarRentalContext>, IRentalDal
+    {
+        public List<RentalDetailsDto> GetRentalDetails()
+        {
+            using (EfCarRentalContext context = new EfCarRentalContext())
+            {
+                var result = from r in context.Rentals
+                             join c in context.Cars on r.CarId equals c.CarId
+                             join b in context.Brands on c.BrandId equals b.BrandId
+                             join col in context.Colors on c.ColorId equals col.ColorId
+                             join user in context.Users on r.CustomerId equals user.Id
+
+                             select new RentalDetailsDto
+                             {
+                                 RentalId=r.Id,
+                                 CarRentDate = r.RentDate,
+                                 CarReturnDate = r.ReturnDate,
+
+                                 CarId = c.CarId,
+                                 CarName = c.CarName,
+                                 CarModelYear = c.ModelYear,
+                                 CarDescription = c.Description,
+                                 CarDailyPrice = c.DailyPrice,
+
+                                 BrandName = b.BrandName,
+                                 
+                                 ColorName = col.ColorName,
+
+                                 CustomerId = user.Id,
+                                 FirstName = user.FirstName,
+                                 LastName = user.LastName
+                             };
+                return result.ToList();
+
+            }
+        }
+    }
+}
